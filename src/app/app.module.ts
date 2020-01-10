2fb18e477c3bb8479d3dfa4b478c1b10752bdd42
TLongueira <tiagolongueira@gmail.com>
2020-01-10 18:55:55 -0300

    Importado modulo en typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HolaMundo} from './primercomponente/primer.componente';
 
 @NgModule({
   declarations: [
     AppComponent
+    HolaMundo
   ],
   imports: [
     BrowserModule
