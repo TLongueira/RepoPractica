991e42fe0092bea96213771fe3bb45483e4eaf81
TLongueira <tiagolongueira@gmail.com>
2020-01-10 19:28:18 -0300

    Commit con dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HolaMundo} from './primercomponente/primer.componente';
+import { SegundocomponenteComponent } from './segundocomponente/segundocomponente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaMundo
+    HolaMundo,
+    SegundocomponenteComponent
   ],
   imports: [
     BrowserModule
--- src/app/primercomponente/primer.componente.ts
+++ src/app/primercomponente/primer.componente.ts
@@ -2,8 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'holamundo',
-    template:'<h1>Hola Mundo</h1>'
+    templateUrl:'./primer.componente.html',
+    styleUrls:['./primer.component.css']
 })
 export class HolaMundo {
-
+    title ='Primer Componente'
 }
